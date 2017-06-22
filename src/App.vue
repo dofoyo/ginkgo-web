42bde5db51ef13f2166140a8c5ad2347cd4661b0
dofoyo <dofoyo@sina.cn>
2017-06-22 13:02:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <div class="logo">
+       <img src="./assets/logo.png">
+    </div> 
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+
+#app .logo{
+  text-align: center;
+}
+</style>
